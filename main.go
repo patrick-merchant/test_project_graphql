b2030031534f494f841ffde92523f855d57b96da
Patrick Merchant <patrickmerchant9@gmail.com>
2022-07-26 16:32:13 +0100

    handles cors error

--- main.go
+++ main.go
@@ -151,10 +151,24 @@ func parseSchema(path string, resolver interface{}) *graphql.Schema {
 	return parsedSchema
 }
 
-func main() {
-	http.Handle("/graphql", &relay.Handler{
-		Schema: parseSchema("./schema.graphql", &RootResolver{}),
+// CORS
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
 	})
+}
+
+func main() {
+	http.Handle("/graphql", CorsMiddleware(&relay.Handler{
+		Schema: parseSchema("./schema.graphql", &RootResolver{}),
+	})) 
 
 	fmt.Println("serving on 8081")
 	log.Fatal(http.ListenAndServe(":8081", nil))
